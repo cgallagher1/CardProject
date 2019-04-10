0a5178e8c1bb65a79d866e50bfb50afff7193b23
Charles Gallagher <cgallagher1@carthage.edu>
2019-04-09 19:18:06 -0500

    Commented Main

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 int main()
 {
+	//Allows the Program to Randomize
 	srand(unsigned(time(0)));
     cout << "Hello World!!!" << endl;
 
@@ -21,8 +22,10 @@ int main()
     return 0;
 }
 
+//Test Cards will test all the functionallity of the card class
 TEST_CASE("Test Cards")
 {
+	//Creates bad cards
     SECTION("Test create bad cards")
     {
         Card c1(-10, "Hearts");
@@ -37,6 +40,7 @@ TEST_CASE("Test Cards")
         REQUIRE(c3.getSuit() == "Clubs");
     }
 
+	//Makes sure that we are creating all 52 unqiue cards
     SECTION("Test create 52 cards")
     {
         string suits[] = {"Clubs", "Diamonds", "Hearts", "Spades"};
@@ -51,6 +55,7 @@ TEST_CASE("Test Cards")
         }        
     }  
      
+	//Makes sure that the card can print itself correctly if needed
     SECTION("Test toString()")
     {
         Card c1(3, "Diamonds");
@@ -62,19 +67,24 @@ TEST_CASE("Test Cards")
         REQUIRE(c3.toString() == "Ace of Spades");
     }
 
+	//Test to see if two cards are equal
     SECTION("Test equivalence")
     {
         Card c1(3, "Diamonds");
         Card c2(3, "Diamonds");
         Card c3(14, "Spades");
 
+		//3 of Diamonds is equal to 3 of Diamonds
         REQUIRE(c1.equal(c2) == true);
+		//3 of Diamonds is not eaul to Ace of Space
         REQUIRE(c1.equal(c3) == false);
     }
 }
 
+//Tests all the functionallity of the Deck class
 TEST_CASE("Test Deck")
 {
+	//When a Deck is created, it should have 52 cards in it
     SECTION("Test create Deck")
     {
         Deck deck1;
@@ -83,6 +93,7 @@ TEST_CASE("Test Deck")
         REQUIRE(deck1.getLength() == 52);
     }
 
+	//After being shuffled, two decks should not be the same
     SECTION("Test shuffle Deck")
     {
         Deck deck1;
@@ -95,6 +106,8 @@ TEST_CASE("Test Deck")
         REQUIRE(deck1.equal(deck2) == false);
     }
 
+	//After dealing, cards should be removed from the deck
+	//For example, after dealing 5 cards a deck of 52 should only have 47
     SECTION("Test Dealing")
     {
         Deck deck1;
@@ -112,6 +125,7 @@ TEST_CASE("Test Deck")
 
     }
 
+	//Checks to make sure there are 52 unique cards in a Deck
 	SECTION("Validation")
 	{
 		Deck deck1;
@@ -122,6 +136,7 @@ TEST_CASE("Test Deck")
     
 }
 
+//Tests the functionallity of the Poker Evaluater Clas
 TEST_CASE("Test Evaluator")
 {
 	SECTION("Testing setup")
@@ -134,12 +149,12 @@ TEST_CASE("Test Evaluator")
 		//7 hand poker check
 		REQUIRE(pe.getHand().size() == 7);
 
-		//Testing each hand
-
-	    
+		//Testing each Deal, to test we set retval to false
+		//and we flip it to true if we find our desired Hand
 		bool retval = false;
 
-		//pairs
+		//Pairs should happen every other hand so we should see
+		//one pair in five deals
 		for (int i = 0; i < 5; i++)
 		{			
 			if (pe.isPair())
@@ -151,7 +166,8 @@ TEST_CASE("Test Evaluator")
 		REQUIRE(retval == true);
 		retval = false;
 
-		//two pair
+		//Two pair should happen every 21 Hands so we should see
+		//a two pair in 25 deals
 		for (int i = 0; i < 25; i++)
 		{
 			if (pe.isTwoPair())
@@ -164,7 +180,8 @@ TEST_CASE("Test Evaluator")
 		retval = false;
 
 
-		//three OAK
+		//Three Of A Kind happens every 47 Hands so we should see
+		//Three Of A Kind in 50 deals
 		for (int i = 0; i < 50; i++)
 		{
 			if (pe.isThreeOfAKind())
@@ -177,7 +194,8 @@ TEST_CASE("Test Evaluator")
 		retval = false;
 
 
-		//straight
+		//A Straight happens every 254 Hands so we should see
+		//a Straight in 300 deals
 		for (int i = 0; i < 300; i++)
 		{
 			if (pe.isStraight())
@@ -190,7 +208,8 @@ TEST_CASE("Test Evaluator")
 		retval = false;
 
 
-		//flush
+		//A Flush happends every 508 Hands so we should see a Flush
+		//in 600 deals
 		for (int i = 0; i < 600; i++)
 		{
 			if (pe.isFlush())
@@ -202,7 +221,8 @@ TEST_CASE("Test Evaluator")
 		REQUIRE(retval == true);
 		retval = false;
 
-		//full house
+		//A Full house happends every 693 Hands so we should see
+		//a Flush in 800 deals
 		for (int i = 0; i < 800; i++)
 		{
 			if (pe.isFullHouse())
@@ -214,7 +234,8 @@ TEST_CASE("Test Evaluator")
 		REQUIRE(retval == true);
 		retval = false;
 
-		//four of a kind
+		//A Four of a kind happends every 4,165 Hands so we should
+		//see a Flush in 50000 deals 
 		for (int i = 0; i < 5000; i++)
 		{
 			if (pe.isFourOfAKind())
@@ -226,7 +247,8 @@ TEST_CASE("Test Evaluator")
 		REQUIRE(retval == true);
 		retval = false;
 
-		//Straight flush
+		//A Straight flush happends every 72,192 Hands so we should
+		//see a Straight Flush in 80000 deals
 		for (int i = 0; i < 80000; i++)
 		{
 			if (pe.isStraightFlush())
@@ -238,7 +260,8 @@ TEST_CASE("Test Evaluator")
 		REQUIRE(retval == true);
 		retval = false;
 
-		//Royal Flush
+		//A Royal Flush happens every 649,740 Hands so we should
+		//see a Royal Flush in 1000000 deals
 		for (int i = 0; i < 1000000; i++)
 		{
 			if (pe.isRoyalFlush())
@@ -251,7 +274,7 @@ TEST_CASE("Test Evaluator")
 
 
 
-		//Evaluate
+		//Forces a High Card
 		vector<Card> testHighCard = {
 			Card(2, "Spades"),
 			Card(4, "Hearts"),
@@ -264,6 +287,7 @@ TEST_CASE("Test Evaluator")
 
 		REQUIRE(pe.evaluate(testHighCard) == "High Card");
 
+		//Forces a Pair
 		vector<Card> testPair = {
 			Card(2, "Spades"),
 			Card(4, "Hearts"),
@@ -276,6 +300,7 @@ TEST_CASE("Test Evaluator")
 
 		REQUIRE(pe.evaluate(testPair) == "Pair");
 
+		//Forces a Straight
 		vector<Card> testStraight = {
 			Card(2, "Spades"),
 			Card(9, "Hearts"),
@@ -288,6 +313,7 @@ TEST_CASE("Test Evaluator")
 
 		REQUIRE(pe.evaluate(testStraight) == "Straight");
 
+		//Forces a Flush
 		vector<Card> testFlush = {
 			Card(5, "Hearts"),
 			Card(9, "Hearts"),
@@ -300,6 +326,7 @@ TEST_CASE("Test Evaluator")
 
 		REQUIRE(pe.evaluate(testFlush) == "Flush");
 
+		//Forces a Full House
 		vector<Card> testFullHouse = {
 			Card(3, "Hearts"),
 			Card(3, "Clubs"),
@@ -312,6 +339,7 @@ TEST_CASE("Test Evaluator")
 
 		REQUIRE(pe.evaluate(testFullHouse) == "Full House");
 
+		//Forces a Royal Flush
 		vector<Card> testRoyalFlush = {
 			Card(10, "Spades"),
 			Card(12, "Spades"),
